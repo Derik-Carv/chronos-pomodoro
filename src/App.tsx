5069083f688e3287cd7a73436b75134041ec8fc0
Derik-Carv <derikcarv.dc@gmail.com>
2026-04-04 17:12:45 -0300

    add-style-heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './componentes/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { Trash2 } from 'lucide-react';
 
 export const App = () => {
     return (
         <>
-            <Heading>Olá mundo!</Heading>
+            <Heading >
+                Olá mundo!
+                <button>
+                    <Trash2 />
+                </button>
+            </Heading>
             <p>Este é o componente App.</p>
         </>
     )
--- src/componentes/Heading.tsx
+++ src/componentes/Heading.tsx
@@ -1,10 +1,15 @@
+import type React from 'react';
 import styles from './Heading.module.css';
 
-export const Heading = (props) => {
+type HeadingProps = {
+    children: React.ReactNode;
+}
+
+export const Heading = ({ children }: HeadingProps) => {
     const headingClass = `${styles.heading}`;
     return (
         <>
-            <h1 className={headingClass}>{props.children}</h1>
+            <h1 className={headingClass}>{children}</h1>
         </>
     )
 };
